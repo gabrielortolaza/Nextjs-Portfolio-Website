ebcade1898366a2d9f04b9febce1dbd166ad9ff4
Leon <leon.jsmith0208@gmail.com>
2025-02-11 08:45:24 -0500

    Update a text

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -7,7 +7,7 @@ export const Cta = () => {
       <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <p className="text-xl font-medium lg:text-2xl">
-            Let's discuss and create the solutions your business needs.
+            Let&apos;s discuss and create the solutions your business needs.
           </p>
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
